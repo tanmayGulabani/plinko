d63367a09dbe84383a6045cd016e9e7d83b69c09
tanmayGulabani <75249050+tanmayGulabani@users.noreply.github.com>
2021-01-23 12:29:14 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,91 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine, world;
+
+var particle;
+var divisions = [];
+var particles = [particle];
+var plinkos = [];
+var line;
+
+var divisionHeight = 300;
+
+var gameState = "PLAY";
+
+var count = 0;
+var score = 0;
+
+function setup() {
+    createCanvas(800, 800);
+    engine = Engine.create();
+    world = engine.world;
+    ground = new Ground(width/2,height,width,20);
+  
+  
+     for (var k = 0; k <=width; k = k + 80) {
+       divisions.push(new Division(k, height-divisionHeight/2, 10, divisionHeight));
+     }
+  
+  
+      for (var j = 75; j <=width; j=j+50) 
+      {
+      
+         plinkos.push(new Plinko(j,75));
+      }
+  
+      for (var j = 50; j <=width-10; j=j+50) 
+      {
+      
+         plinkos.push(new Plinko(j,175));
+      }
+  
+       for (var j = 75; j <=width; j=j+50) 
+      {
+      
+         plinkos.push(new Plinko(j,275));
+      }
+  
+       for (var j = 50; j <=width-10; j=j+50) 
+      {
+      
+         plinkos.push(new Plinko(j,375));
+      }
+  
+      
+  
+      
+  }
+
+ 
+
+function draw() {
+    background(0);
+    Engine.update(engine);
+    textSize(35)
+  text("Score : "+score,20,40);
+  fill("red");
+  
+  textSize(35)
+  text(" 500 ", 5, 550);
+  text(" 100 ", 480, 550);
+  text(" 200 ", 560, 550);
+  
+
+
+
+    ground.display();
+       
+      } 
+      for(var k = 0; k < plinkos.length; k++) {
+       plinkos[k].display();
+    }\
+ for (var i = 0; i < divisions.length; i++) {
+     
+    divisions[i].display();
+  }
+  
+}
+
+
